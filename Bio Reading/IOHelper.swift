45530cb855a3516332e161891fde09f04b03c4f0
Andrew Breckenridge <asbreckenridge@me.com>
2015-02-06 23:00:22 -0600

    add IO Helper

---
+++ Bio Reading/IOHelper.swift
@@ -0,0 +1,19 @@
+//
+//  IOHelper.swift
+//  Bio Reading
+//
+//  Created by Andrew Breckenridge on 2/6/15.
+//  Copyright (c) 2015 Andrew Breckenridge. All rights reserved.
+//
+
+import Foundation
+
+func getPrompt(file: String, index: Int) -> String {
+    if let plist = NSBundle.mainBundle().pathForResource("\(file).plist", ofType: "plist") {
+        let rootDict = NSArray(contentsOfFile: plist)!
+        
+        return rootDict[0][index] as String
+    } else {
+        return "error in getting prompt"
+    }
+}
