f256158b33dce53ddd407bb9d72bd4cba074f4a5
Andrew Breckenridge <asbreckenridge@me.com>
2015-02-25 17:47:12 -0600

    add an instruction page

---
+++ Bio Reading/InstructionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  InstructionViewController.swift
+//  Bio Reading
+//
+//  Created by Andrew Breckenridge on 2/25/15.
+//  Copyright (c) 2015 Andrew Breckenridge. All rights reserved.
+//
+
+import UIKit
+
+class InstructionViewController: UIViewController {
+
+}
