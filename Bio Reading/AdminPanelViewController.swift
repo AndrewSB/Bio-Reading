6ab7700ce8deae0a4523d7c63c52704a0ff466f5
Andrew Breckenridge <asbreckenridge@me.com>
2015-07-01 22:51:28 -0700

    make the upload incremental

--- Bio Reading/AdminPanelViewController.swift
+++ Bio Reading/AdminPanelViewController.swift
@@ -115,9 +115,12 @@ class AdminPanelViewController: UIViewController, UITableViewDataSource, UITable
         for query in queries {
             let foundObjects = query.findObjects() as! [PFObject]
             for object in foundObjects {
-                object.save()
-                object.unpin()
-                savedObjects++
+                object.saveInBackgroundWithBlock({ (success: Bool, error: NSError?) -> Void in
+                    println("lol \(object)")
+                    savedObjects++
+                })
+//                object.unpin()
+                println(savedObjects)
             }
         }
         
