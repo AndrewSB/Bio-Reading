a1873d56f086c953680c0299c881318886878168
Andrew Breckenridge <asbreckenridge@me.com>
2015-07-06 23:47:36 -0700

    xcdatamodel is *gucci*, as the kids would say

--- Bio Reading/Record.swift
+++ Bio Reading/Record.swift
@@ -1,31 +1,57 @@
 //
-//  Bio_Reading.swift
-//  Bio Reading
+//  Record.swift
+//  
 //
 //  Created by Andrew Breckenridge on 7/6/15.
-//  Copyright (c) 2015 Andrew Breckenridge. All rights reserved.
+//
 //
 
 import Foundation
 import CoreData
+import Parse
 
 class Record: NSManagedObject {
 
-    @NSManaged var audioFile: NSData
-    @NSManaged var bioPerson: String
-    @NSManaged var condition: NSNumber
-    @NSManaged var cue: NSNumber
-    @NSManaged var curiosity: NSNumber
-    @NSManaged var dateTime: NSDate
-    @NSManaged var familiarity: NSNumber
-    @NSManaged var order: NSNumber
-    @NSManaged var readingTime: NSNumber
-    @NSManaged var rTCond: NSNumber
-    @NSManaged var subjectNumber: NSNumber
-
+    @NSManaged var audioFile: NSData!
+    @NSManaged var bioPerson: String!
+    @NSManaged var condition: NSNumber!
+    @NSManaged var cue: NSNumber!
+    @NSManaged var curiosity: NSDecimalNumber!
+    @NSManaged var dateTime: NSDate!
+    @NSManaged var familiarity: NSDecimalNumber!
+    @NSManaged var order: NSNumber!
+    @NSManaged var readingTime: NSNumber!
+    @NSManaged var rTCond: String!
+    @NSManaged var subjectNumber: NSNumber!
+    @NSManaged var cpidr: NSNumber!
+    @NSManaged var sentance: String!
+    
     var stringified: String {
         get {
             return join(",", [subjectNumber, curiosity, familiarity, bioPerson, cue, order, rTCond, dateTime, readingTime].map({"\($0)"}))
         }
     }
+    
+    var parsified: PFObject {
+        get {
+            let object = PFObject(className: "Subject\(subjectNumber)")
+            
+            object["CPIDR"] = cpidr
+            object["sentance"] = sentance
+            
+            let fileName = "Audio\(subjectNumber)_\(bioPerson)_\(cue)".stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.alphanumericCharacterSet())
+            object["audioFile"] = PFFile(name: fileName, data: audioFile, contentType: "audio/wav")
+            
+            object["bioPerson"] = bioPerson
+            object["curiosity"] = curiosity
+            object["dateTime"] = dateTime
+            object["familiarity"] = familiarity
+            object["order"] = order
+            object["readingTime"] = readingTime
+            object["subjectNumber"] = subjectNumber
+            
+            return object
+        }
+    }
+
 }
